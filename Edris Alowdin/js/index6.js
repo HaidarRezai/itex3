52ea2c3e196e798b5c96094b946754739b790169
Edris161 <edrisalowdin2022@gmail.com>
2025-01-15 11:07:57 +0430

    edris

---
+++ Edris Alowdin/js/index6.js
@@ -0,0 +1,9 @@
+function hidden_window(){
+    document.getElementById("popup"). classList. remove(showpopup);
+}
+function light(){
+    document.getElementById("root"). classList. remove("darkMode");
+}
+function dark(){
+    document.getElementById("root"). classList. add("darkMode");
+}
